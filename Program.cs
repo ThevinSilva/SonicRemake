20fb0913a9f67b657963397ea16ba70382225e5d
Lucas <lucas@somfic.dev>
2024-09-21 18:32:05 +0200

    Added basic Sonic

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SFML.Graphics;
+using SFML.Window;
+
+var window = new RenderWindow(new VideoMode(200, 200), "Title");
+var circle = new CircleShape(100);
+circle.FillColor = Color.Red;
+
+var idleTexture = new Texture("idle_1.png");
+var sonic = new Sprite();
+sonic.Texture = idleTexture;
+
+while (window.IsOpen)
+{
+	window.DispatchEvents();
+	
+	window.Clear();
+	window.Draw(sonic);
+	window.Display();
+}
