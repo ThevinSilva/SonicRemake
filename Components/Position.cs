da4317591ef5f772bfa25f52392671c7331f3711
Lucas <lucas@somfic.dev>
2024-10-08 15:33:53 +0200

    ✨ Added `SpriteSheet` component
    semver: minor

--- Components/Position.cs
+++ Components/Position.cs
@@ -7,4 +7,5 @@ namespace SonicRemake.Components;
 public record struct Transform(Vector2f Position, Vector2f Scale, float Rotation);
 public record struct Velocity(float X, float Y);
 public record struct Sprite(string SpriteId);
+public record struct SpriteSheet(int SpriteIndex, int SpriteWidth, int SpritesPerRow);
 public record struct Renderer(Texture? Texture);
