7017309cb03e30a0a3348cc556bf57eb9bf3d715
Thevin Silva <thevinsilva@Thevins-MacBook-Air.local>
2024-10-09 21:07:17 +0100

    Vertical and Horizontal Movement Implemented

--- Movement/Movement.cs
+++ Movement/Movement.cs
@@ -1,41 +1,125 @@
-﻿namespace SonicRemake.Movement;
+﻿
+namespace SonicRemake.Movement;
+
+// IMPLEMENT CONTROL LOCK https://info.sonicretro.org/SPG:Running [bottom]
 
 public class Movement
 {
+    public float Xpos { set; get; }
+    public float Ypos { set; get; }
+
+
+    // Horizontal Movement Constants https://info.sonicretro.org/SPG:Running
     private const float ACCELERATION_SPEED = 0.046875f;
     private const float DECELERATION_SPEED = 0.5f;
     private const float FRICTION_SPEED = ACCELERATION_SPEED;
     private const float TOP_SPEED = 6;
-
-    public Direction CurrentDirrection { get; set;}
     public float GroundSpeed { private set; get; }
 
-    public Movement()
+
+    // Vertical Movement Constants https://info.sonicretro.org/SPG:Air_State 
+    private const float AIR_ACCELERATION_SPEED = ACCELERATION_SPEED * 2;
+    private const float GRAVITY = 0.21875f;
+    private const float JUMP_FORCE = 6.5f;
+    public bool OnGround { set; get; }
+    public float XSpeed { set; get; }
+    public float YSpeed { set; get; }
+    public bool ControlLock { set; get; }
+
+    public Movement(float xpos, float ypos)
     {
         GroundSpeed = 0;
+        XSpeed = 0;
+        Xpos = xpos;
+        Ypos = ypos;
     }
 
-    public void HandleMovement(HashSet<Direction> inputs) 
+    public void HandleMovement(HashSet<Direction> inputs, ushort groundAngle)
     {
-        if (GroundSpeed > 0 && CurrentDirrection != Direction.Forward) CurrentDirrection = Direction.Forward;
-        if (GroundSpeed < 0 && CurrentDirrection != Direction.Backward) CurrentDirrection = Direction.Backward;
+        HandleHorizontalMovement(inputs.Contains(Direction.Backward), inputs.Contains(Direction.Forward));
 
+        HandleVerticalMovement(
+            inputs.Contains(Direction.Space),
+            inputs.Contains(Direction.Backward),
+            inputs.Contains(Direction.Forward)
+        );
 
-        if (inputs.Contains(Direction.Up)) 
+        if (OnGround)
         {
-            // look up 
-        };
-
-        if (inputs.Contains(Direction.Forward))
-        {
-            GroundSpeed += GroundSpeed <= 0 ? DECELERATION_SPEED : ACCELERATION_SPEED;
+            XSpeed = GroundSpeed * (float)Math.Cos(groundAngle);
+            YSpeed = GroundSpeed * -(float)Math.Sin(groundAngle);
         }
 
-        if (inputs.Contains(Direction.Backward))
+        Xpos += XSpeed;
+        Ypos += XSpeed;
+    }
+
+    public void HandleHorizontalMovement(bool backward, bool forward)
+    {
+
+        if (forward)
         {
-            GroundSpeed -= GroundSpeed >= 0 ? DECELERATION_SPEED : ACCELERATION_SPEED;
+            if (GroundSpeed < 0) // going backwards
+            {
+                GroundSpeed += DECELERATION_SPEED;
+
+                if (GroundSpeed >= 0)
+                    GroundSpeed = DECELERATION_SPEED;
+            }
+            else if (GroundSpeed < TOP_SPEED)
+            {
+                GroundSpeed += ACCELERATION_SPEED;
+
+                if (GroundSpeed >= TOP_SPEED)
+                    GroundSpeed = TOP_SPEED; //impose top speed limit
+            }
         }
+        else if (backward)
+        {
+            if (GroundSpeed > 0) // going forwards
+            {
+                GroundSpeed -= DECELERATION_SPEED;
+
+                if (GroundSpeed <= 0)
+                    GroundSpeed = -DECELERATION_SPEED;
+            }
+            else if (GroundSpeed > -TOP_SPEED)
+            {
+                GroundSpeed -= ACCELERATION_SPEED;
+
+                //impose top speed limit
+                if (GroundSpeed <= -TOP_SPEED)
+                    GroundSpeed = -TOP_SPEED;
+            }
+        }
+        // no horizontal movement
+        else
+        {
+            GroundSpeed -= Math.Sign(GroundSpeed) * FRICTION_SPEED;
+        }
+    }
+
+    public void HandleVerticalMovement(bool space, bool backward, bool forward)
+    {
+        if (!OnGround)
+        {
+            // Air Drag Added
+            if (YSpeed < 0 && YSpeed > -4)
+                XSpeed -= (XSpeed / 0.125f) / 256;
+
+            // Gravity Force Added
+            YSpeed = YSpeed > 16 ? 16 : YSpeed + GRAVITY;
+
+            if (forward)
+                XSpeed += AIR_ACCELERATION_SPEED;
+
+            if (backward)
+                XSpeed -= AIR_ACCELERATION_SPEED;
+        }
+
 
 
     }
+
+
 }
