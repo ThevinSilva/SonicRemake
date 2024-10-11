9ac16e5b912520053a24d3e694eb7ce845abd3ef
Lucas <lucas@somfic.dev>
2024-10-11 13:16:29 +0200

    ⚡️ Improve camera handling
    semver: patch

--- Components/Position.cs
+++ Components/Position.cs
@@ -3,23 +3,32 @@ using SFML.Graphics;
 using SFML.System;
 using SonicRemake.Animations;
 
-namespace SonicRemake.Components
+namespace SonicRemake.Components;
+
+public record struct Transform(Vector2f Position, Vector2f Scale, bool IsOnGround = false, float Rotation = 0, ushort GroundAngle = 0);
+
+public record struct Velocity(Vector2f Speed, float GroundSpeed);
+
+public record struct Sprite(string SpriteId, params Color[] MaskColors);
+
+public record struct SpriteSheet(int X, int Y, int SpriteSize);
+
+public record struct Renderer(Texture? Texture, bool FlipX = false, bool FlipY = false);
+
+public record struct SpriteAnimator(AnimationData AnimationData, int SpritesLeft, int FramesLeft, int LoopsLeft);
+
+public record struct SpriteAnimation(string Animation = "idle", int FramesPerSprite = 6);
+
+public record struct Sonic(SonicState State);
+
+public record struct Camera(float Zoom = 4f);
+
+public enum SonicState
 {
-  public record struct Transform(Vector2f Position, Vector2f Scale, bool IsOnGround = false, float Rotation = 0, ushort GroundAngle = 0);
-
-  public record struct Velocity(Vector2f Speed, float GroundSpeed);
-
-  public record struct Sprite(string SpriteId, params Color[] MaskColors);
-
-  public record struct SpriteSheet(int X, int Y, int SpriteSize);
-
-  public record struct Renderer(Texture? Texture, bool FlipX = false, bool FlipY = false);
-
-  public record struct SpriteAnimator(AnimationData AnimationData, int SpritesLeft, int FramesLeft, int LoopsLeft);
-
-  public record struct SpriteAnimation(string Animation = "idle", int FramesPerSprite = 6);
-
-  public record struct Sonic();
-
-  public record struct Camera(float Zoom = 4f);
+  Idle,
+  Running,
+  Skidding,
+  Jumping,
+  Falling,
+  Charging,
 }
