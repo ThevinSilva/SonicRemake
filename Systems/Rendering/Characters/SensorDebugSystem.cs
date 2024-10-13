be3beab2469fd4428457de727572970e9eac6428
Lucas <lucas@somfic.dev>
2024-10-13 13:51:31 +0200

    🐛 Add camera offset to `SensorDebugSystem`
    semver: patch

--- Systems/Rendering/Characters/SensorDebugSystem.cs
+++ Systems/Rendering/Characters/SensorDebugSystem.cs
@@ -14,9 +14,19 @@ public class SensorDebug : GameSystem
 	private static Log _log = new(typeof(SonicAnimationSystem));
 
 	private QueryDescription Query = new QueryDescription().WithAll<Sonic, Transform, SpriteSheet>();
+	private QueryDescription CameraQuery = new QueryDescription().WithAll<Components.Camera, Transform>();
 
 	public override void OnRender(World world, RenderWindow window, GameContext context)
 	{
+		var cameraPosition = new Vector2f();
+		var cameraZoom = 1f;
+
+		world.Query(in CameraQuery, (Entity entity, ref Components.Camera camera, ref Transform transform) =>
+		{
+			cameraPosition = transform.Position;
+			cameraZoom = camera.Zoom;
+		});
+
 		world.Query(in Query, (
 		Entity entity,
 		ref Sonic sonic,
@@ -31,12 +41,25 @@ public class SensorDebug : GameSystem
 			_log.Debug(origin);
 			_log.Debug(widthRadius);
 
+			// FIXME: This functionality is repeated in the RenderSystem, it
+			//        should probably be extracted to a helper function somewhere
+			origin.X -= cameraPosition.X;
+			origin.Y -= cameraPosition.Y;
+
+			origin.X *= cameraZoom;
+			origin.Y *= cameraZoom;
+
+			origin.X += window.Size.X / 2;
+			origin.Y += window.Size.Y / 2;
+
+			var scale = new Vector2f(1, 1) * cameraZoom;
 
 			window.Draw(new RectangleShape()
 			{
 				FillColor = new SFML.Graphics.Color(255, 0, 0),
 				Size = new Vector2f(widthRadius, 1),
 				Position = origin,
+				Scale = scale,
 			});
 
 		});
