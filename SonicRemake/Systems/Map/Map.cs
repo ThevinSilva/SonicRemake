14df35f328a292643d44b901ab6818805025b571
Lucas <lucas@somfic.dev>
2025-03-27 00:40:33 +0100

    🚚 move `Map` to systems
    semver: chore

--- SonicRemake/Systems/Map/Map.cs
+++ SonicRemake/Systems/Map/Map.cs
@@ -22,17 +22,18 @@ public class TileManagementSystem : GameSystem
 	private Tile[] SolidTiles { get; set; }
 	private uint[,] TileMap { get; set; }
 
-	private Map _map;
+	private List<Entity> entities = [];
+
 
 	public override void OnStart(World world)
 	{
 		world.Query(in Query, (ref SolidTiles map) =>
 		{
 			using var stream = File.OpenRead(FILENAME);
-			_map = Map.FromStream(stream, ts => File.OpenRead(Path.Combine(Path.GetDirectoryName(FILENAME), ts.Source)));
+			Map solidMap = Map.FromStream(stream, ts => File.OpenRead(Path.Combine(Path.GetDirectoryName(FILENAME), ts.Source)));
 
 			SolidTiles = LoadSolidTiles("./Assets/Map/Tiles.json");
-			TileMap = LoadMap();
+			TileMap = LoadMap(solidMap);
 
 			map.TileMap = TileMap;
 			map.TileSet = SolidTiles;
@@ -51,10 +52,10 @@ public class TileManagementSystem : GameSystem
 		return tileset;
 	}
 
-	private uint[,] LoadMap()
+	private uint[,] LoadMap(Map tiledMap)
 	{
 		// Base layer that contains the solidTile data
-		var layer = _map.Layers.OfType<TileLayer>().First();
+		var layer = tiledMap.Layers.OfType<TileLayer>().First();
 		uint[,] map = new uint[layer.Height, layer.Width];
 
 		for (int y = 0, i = 0; y < layer.Height; y++)
@@ -68,6 +69,8 @@ public class TileManagementSystem : GameSystem
 
 	public void CreateDrawableEntities(World world)
 	{
+		entities.Clear();
+
 		for (int y = 0; y < TileMap.GetLength(0); y++)
 		{
 			for (int x = 0; x < TileMap.GetLength(1); x++)
@@ -84,11 +87,13 @@ public class TileManagementSystem : GameSystem
 				var scale = MapUtil.GetTransformedVector(TileMap[y, x]);
 				var image = new Image(SolidTiles[id > 0 ? id - 1 : id].GetColorMatrix());
 
-				world.Create(
+				var entity = world.Create(
 				new Components.Transform(pos, scale),
 					new Renderer(Layer.BackgroundTiles),
 					new SpriteTexture(new Texture(image))
 				);
+
+				entities.Add(entity);
 			}
 		}
 	}
